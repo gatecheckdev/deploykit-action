958351bc4d6339ff6b24d79f44285bda000b7edd
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-04-25 20:15:55 -0500

    fix: add safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # git config --global user.name $DK_MANIFEST_USER
 # git config --global user.email $DK_MANIFEST_EMAIL
+git config --global --add safe.directory /github/workspace
 
 echo "[INFO] List manifest directory"
 
